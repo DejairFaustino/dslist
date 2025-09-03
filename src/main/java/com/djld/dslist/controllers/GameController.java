6c70ce0b874056b42abff659b9d64a27f87bb108
Dejair Faustino <dejair.f@gmail.com>
2025-09-03 12:43:59 -0300

    GET game by id

--- src/main/java/com/djld/dslist/controllers/GameController.java
+++ src/main/java/com/djld/dslist/controllers/GameController.java
@@ -6,23 +6,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.djld.dslist.dto.GameDTO;
 import com.djld.dslist.dto.GameMinDTO;
 import com.djld.dslist.services.GameService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController // Configurando essa classe para ser um controlador
-@RequestMapping(value = "/games") // maapeando o recurso que neste caso é "games", ou seja, mapeamento do caminho para a API (end point)
+@RequestMapping(value = "/games") // maapeando o recurso que neste caso é "games", ou seja, mapeamento do caminho para a API (end point) carregar todos os resultados
 public class GameController {
 
     @Autowired
     private GameService gameService; // injetando a camada de serviços nesta classe 
 
-    @GetMapping
-    public List<GameMinDTO> findAll() {
-        List<GameMinDTO> result = gameService.findAll();
+    @GetMapping(value = "/{id}") // mapeando o recurso que neste caso é "/id", ou seja, mapeamento do caminho para a API (end point) carregar o resultado a partir do "id" informado
+    public GameDTO findById(@PathVariable Long id) { // o "@PathVariable" faz com que o "id" do argumento case com o "{id}" da requisição
+        GameDTO result = gameService.findById(id);
         return result;
     }
     
+    @GetMapping
+    public List<GameMinDTO> findAll() { //gerando o resultado que será carregado com a requsição "/game"
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
 }
--- src/main/java/com/djld/dslist/services/GameService.java
+++ src/main/java/com/djld/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.djld.dslist.dto.GameDTO;
 import com.djld.dslist.dto.GameMinDTO;
 import com.djld.dslist.entities.Game;
 import com.djld.dslist.repositories.GameRepository;
@@ -15,10 +17,21 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository; //Injetando uma instância do GameRepository para que seja possível buscar as consultas
 
+    @Transactional(readOnly = true) // A inserção dessa anotation Sprning em todos os métodos de serviços é um  boa pratica para garantir que o banco de dados vai obedecer aos processo de transações
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        /*Forma padrão de carregar o resultado inserida apenas para fins didáticos
+        GameDTO dto = new GameDTO(result);
+        return dto;*/
+        //FORMA SIMPLES DE REALIZA A MESMA CHAMADA
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
-        //FORMA DIDÁTICA DE CHAMAR O RESULTADO
-        /*List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        /*Forma padrão de carregar o resultado inserida apenas para fins didáticos
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;*/
         //FORMA SIMPLES DE REALIZA A MESMA CHAMADA
         return result.stream().map(x -> new GameMinDTO(x)).toList();
